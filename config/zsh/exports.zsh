72db14b6559cae2a991657635423905d62263865
Eric Shen <eric.yan.shen@gmail.com>
2026-05-29 01:01:38 -0700

    no more lag spikes tyvm

--- config/zsh/exports.zsh
+++ config/zsh/exports.zsh
@@ -23,6 +23,3 @@ export NVM_DIR="$HOME/.nvm"
 export SDKMAN_DIR="$HOME/.sdkman"
 [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
 
-# iterm2 integration
-test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
-
